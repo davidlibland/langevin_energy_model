0743772fdf246a5786ac4040ea340b6a272a7a09
David Li-Bland <david.libland@gmail.com>
2019-11-19 19:58:16 -0800

    Fixes checkpointing

---
+++ hparam_sweep.py
@@ -0,0 +1,203 @@
+import os
+import random
+import time
+from collections import deque
+from typing import Any
+from typing import Callable
+from typing import Tuple
+
+import torch
+from ray.tune import Trainable
+import matplotlib.pyplot as plt
+import torch.utils.data as data
+from torch import optim
+
+from mcmc.mala import MALASampler
+from distributions import core
+from utils.ais import AISLoss
+import model
+
+# Globals (make these configurable)
+
+MAX_REPLAY = 30
+REPLAY_PROB = .99
+
+
+class Logger:
+    """
+    A simple container for logging a variety of metrics.
+    """
+    def __init__(self):
+        self.logs = dict()
+
+    def __call__(self, **kwargs: float):
+        """Logs the metrics passed by keyword, these are stored until flush is called"""
+        for k, v in kwargs.items():
+            if k not in self.logs:
+                self.logs[k] = []
+            self.logs[k].append(v)
+
+    def flush(self):
+        """Clears the store of logs"""
+        self.logs = dict()
+
+    def means(self):
+        """Returns the means of the metrics"""
+        return {
+            k: 0 if not vs else sum(vs)/len(vs)
+            for k, vs in self.logs.items()
+        }
+
+
+def get_energy_trainer(setup_dist: Callable[[Any, ...], Tuple[core.Sampler, model.BaseEnergyModel]]):
+    """Returns a tune trainable for the distribution and model architecture"""
+    class EnergyTrainer(Trainable):
+        def _setup(self, config):
+            self.lr = config.get("lr", 1e-2)
+            self.num_epochs = config.get("num_epochs", 100)
+            self.num_mc_steps = config.get("num_mc_steps", 10)
+            self.num_sample_mc_steps = config.get("num_sample_mc_steps", 1000)
+            self.sample_beta = config.get("sample_beta", 0.1)
+            self.batch_size = config.get("batch_size", 1024)
+            self.verbose = True
+
+            self.dist, self.net_ = setup_dist(**config)
+            samples = self.dist.rvs(10000)
+            print(samples.shape)
+            dataset = data.TensorDataset(torch.tensor(samples, dtype=torch.float))
+            self.energy = lambda x: self.net_(torch.tensor(x, dtype=torch.float)).detach().numpy()
+
+            self.optimizer_ = optim.Adam(self.net_.parameters(), lr=self.lr, weight_decay=1e-1)
+
+            self.sampler = MALASampler(lr=0.1)
+
+            self.dataloader = data.DataLoader(
+                dataset=dataset,
+                batch_size=self.batch_size,
+                drop_last=True,
+                shuffle=True
+            )
+            # Determine the device type:
+            self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            self.net_.to(self.device)
+            print(f"training on {len(dataset)} samples using {self.device}.")
+            self.global_step_ = 0
+            self.epoch_ = 0
+            self.model_samples_ = None
+
+            self.logger_ = Logger()
+            self.ais_loss = AISLoss(logger=self.logger_, log_z_update_interval=9)
+            self.step_callbacks = [self.ais_loss]
+
+        def save_images(self, label=None, dir=""):
+            """Saves sample images at the giving dir"""
+            if label is None:
+                label=self.global_step_
+            fig = plt.figure()
+            self.net_.eval()
+            samples = self.net_.sample_fantasy(x=None,
+                                               num_mc_steps=self.num_sample_mc_steps,
+                                               beta=self.sample_beta,
+                                               mc_dynamics=self.sampler,
+                                               num_samples=36
+                                               ).detach().cpu()
+            self.dist.visualize(fig, samples, self.energy)
+            plot_fn = os.path.join(dir, f"samples_{label}.png")
+            fig.savefig(plot_fn)
+            plt.close(fig)
+
+        def save_model(self, dir="", **kwargs):
+            """Saves the model at the given dir"""
+            ckpt_fn = os.path.join(dir, f"model.pkl")
+            torch.save({
+                    "global_step": self.global_step_,
+                    "epoch": self.epoch_,
+                    "model": self.net_.state_dict(),
+                    "optimizer": self.optimizer_.state_dict(),
+                    "model_samples": list(self.model_samples_)
+                }, ckpt_fn
+            )
+
+        def load_model(self, dir=""):
+            """Loads the model from the specified dir."""
+            ckpt_fn = os.path.join(dir, f"model.pkl")
+            checkpoint = torch.load(ckpt_fn)
+            self.net_.load_state_dict(checkpoint["model"])
+            self.optimizer_.load_state_dict(checkpoint["optimizer"])
+            self.epoch_ = checkpoint["epoch"]
+            self.global_step_ = checkpoint["global_step"]
+            self.model_samples_ = deque(checkpoint["model_samples"])
+
+        def _train(self):
+            """Train the model on one epoch"""
+            objectives = []
+            epoch_training_time = 0
+            epoch_metrics_time = 0
+            self.epoch_ += 1
+            for i_batch, sample_batched in enumerate(self.dataloader):
+                self.global_step_ += 1
+                batch_start_time = time.time()
+                data_sample = sample_batched[0].to(self.device)
+
+                # Get model samples, either from replay buffer or noise.
+                if self.model_samples_ is None:
+                    self.model_samples_ = deque(
+                        [self.net_.sample_from_prior(data_sample.shape[0], device=self.device)])
+                elif len(self.model_samples_) > MAX_REPLAY:
+                    self.model_samples_.popleft()
+                replay_sample = random.choices(
+                    self.model_samples_,
+                    # favor more recent samples:
+                    weights=list(range(1, len(self.model_samples_) + 1))
+                )[0]
+                noise_sample = self.net_.sample_from_prior(replay_sample.shape[0],
+                                                           device=self.device)
+                mask = torch.rand(replay_sample.shape[0]) < REPLAY_PROB
+                while len(mask.shape) < len(replay_sample.shape):
+                    # Add extra feature-dims
+                    mask.unsqueeze_(dim=-1)
+                model_sample = torch.where(mask.to(self.device), replay_sample,
+                                           noise_sample)
+
+                self.net_.eval()
+                model_sample = self.net_.sample_fantasy(model_sample,
+                                                        num_mc_steps=self.num_mc_steps).detach()
+
+                self.model_samples_.append(model_sample)
+
+                # Forward gradient:
+                self.net_.train()
+                self.net_.zero_grad()
+                objective = self.net_(data_sample).mean() - self.net_(model_sample).mean()
+                objective.backward()
+                torch.nn.utils.clip_grad.clip_grad_value_(self.net_.parameters(), 1e2)
+                self.optimizer_.step()
+
+                batch_training_time = time.time() - batch_start_time
+                epoch_training_time += batch_training_time
+                objectives.append(objective)
+
+                tr_metrics_start_time = time.time()
+                for callback in self.step_callbacks:
+                    callback(net=self.net_, data_sample=data_sample,
+                             model_sample=model_sample, epoch=self.epoch_,
+                             global_step=self.global_step_, validation=False)
+                tr_metrics_time = time.time() - tr_metrics_start_time
+                epoch_metrics_time += tr_metrics_time
+                if self.verbose:
+                    print(f"on epoch {self.epoch_}, batch {i_batch}, objective: {objective}")
+                    print(f"training time: {batch_training_time:0.3f}s, metrics time: {tr_metrics_time:0.3f}s")
+            means = self.logger_.means()
+            self.logger_.flush()
+            return means
+
+        def _save(self, tmp_checkpoint_dir):
+            """Save the model"""
+            self.save_model(dir=tmp_checkpoint_dir)
+            self.save_images(dir=tmp_checkpoint_dir)
+            return tmp_checkpoint_dir
+
+        def _restore(self, checkpoint):
+            """Restore the model"""
+            self.load_model(checkpoint)
+    return EnergyTrainer
--- main.py
+++ main.py
@@ -1,28 +1,18 @@
-import csv
-import os
-
 import numpy as np
-import torch
-import torch.optim as optim
-import torch.utils.data as data
-import matplotlib.pyplot as plt
 from ray.tune.schedulers import ASHAScheduler
+from ray import tune
 
 from distributions.core import Distribution, Normal
 from distributions.mnist import get_approx_mnist_distribution
 from distributions.digits import get_approx_digit_distribution
 from distributions.small_digits import get_approx_sm_digit_distribution
 from distributions.small_patterns import get_approx_pattern_distribution
-from mcmc.mala import MALASampler
+from hparam_sweep import get_energy_trainer
 from model import ConvEnergyModel
 from model import SimpleEnergyModel, ResnetEnergyModel
-from training_loop import train
-from utils.ais import AISLoss
-from utils.logging import RUN_DIR
-from ray import tune
 
 
-def setup_1d():
+def setup_1d(**kwargs):
     dist = Distribution.mixture([
         Normal(np.array([-3])),
         Normal(np.array([3])),
@@ -32,7 +22,7 @@ def setup_1d():
     return dist, net
 
 
-def setup_2d():
+def setup_2d(**kwargs):
     dist = Distribution.mixture([
         Normal(np.array([-3, 2])),
         Normal(np.array([3, 5])),
@@ -42,7 +32,7 @@ def setup_2d():
     return dist, net
 
 
-def setup_patterns(patterns=("checkerboard_2x2", "diagonal_gradient_2x2")):
+def setup_patterns(patterns=("checkerboard_2x2", "diagonal_gradient_2x2"), **kwargs):
     dist = get_approx_pattern_distribution(patterns)
     n = 1000
     scale = np.sqrt((dist.rvs(n)**2).sum()/n)
@@ -50,7 +40,7 @@ def setup_patterns(patterns=("checkerboard_2x2", "diagonal_gradient_2x2")):
     return dist, net
 
 
-def setup_sm_digits_simple():
+def setup_sm_digits_simple(**kwargs):
     dist = get_approx_sm_digit_distribution()
     n = 1000
     scale = np.sqrt((dist.rvs(n)**2).sum()/n)
@@ -58,7 +48,7 @@ def setup_sm_digits_simple():
     return dist, net
 
 
-def setup_sm_digits(model="conv", n_hidden=12):
+def setup_sm_digits(model="conv", n_hidden=12, **kwargs):
     dist = get_approx_sm_digit_distribution()
     n = 1000
     scale = np.sqrt((dist.rvs(n)**2).sum()/n)
@@ -69,102 +59,38 @@ def setup_sm_digits(model="conv", n_hidden=12):
     return dist, net
 
 
-def setup_digits(model="conv"):
+def setup_digits(model="conv", n_hidden=25, **kwargs):
     dist = get_approx_digit_distribution()
     n = 1000
     scale = np.sqrt((dist.rvs(n)**2).sum()/n)
     if model == "resnet":
-        net = ResnetEnergyModel((1, 8, 8), 3, 2, 25, prior_scale=5*scale)
+        net = ResnetEnergyModel((1, 8, 8), 3, 2, n_hidden, prior_scale=5*scale)
     else:
-        net = ConvEnergyModel((1, 8, 8), 3, 25, prior_scale=5*scale)
+        net = ConvEnergyModel((1, 8, 8), 3, n_hidden, prior_scale=5*scale)
     return dist, net
 
 
-def setup_mnist():
+def setup_mnist(n_hidden=25, **kwargs):
     dist = get_approx_mnist_distribution()
     n = 1000
     scale = np.sqrt((dist.rvs(n)**2).sum()/n)
-    net = ResnetEnergyModel((1, 28, 28), 3, 2, 25, prior_scale=scale)
+    net = ResnetEnergyModel((1, 28, 28), 3, 2, n_hidden, prior_scale=scale)
     return dist, net
-
-
-def training_entrypoint(config):
-    lr = config.get("lr", 1e-2)
-    num_epochs = config.get("num_epochs", 10)
-    num_mc_steps = config.get("num_mc_steps", 10)
-    num_sample_mc_steps = config.get("num_sample_mc_steps", 1000)
-    sample_beta = config.get("sample_beta", 0.1)
-    n_hidden = config.get("n_hidden", 12)
-    model = config.get("model", "conv")
-    fname = "samples"
-    show = False
-    os.makedirs(RUN_DIR, exist_ok=True)
-
-    # dist, net = setup_digits("resnet")
-    dist, net = setup_sm_digits(model=model, n_hidden=n_hidden)
-    samples = dist.rvs(10000)
-    print(samples.shape)
-    dataset = data.TensorDataset(torch.tensor(samples, dtype=torch.float))
-    energy = lambda x: net(torch.tensor(x, dtype=torch.float)).detach().numpy()
-    fig = plt.figure()
-    dist.visualize(fig, samples, energy)
-    if show:
-        plt.show()
-    if fname:
-        fig.savefig(os.path.join(RUN_DIR, f"{fname}_data.png"))
-    plt.close(fig)
-
-    optimizer = optim.Adam(net.parameters(), lr=lr, weight_decay=1e-1)
-
-    sampler = MALASampler(lr=0.1)
-
-    logdir = tune.track.get_session().logdir
-    def save_images(net):
-        fig = plt.figure()
-        net.eval()
-        samples = net.sample_fantasy(x=None,
-                                     num_mc_steps=num_sample_mc_steps,
-                                     beta=sample_beta,
-                                     mc_dynamics=sampler,
-                                     num_samples=36
-                                     ).detach().cpu()
-        dist.visualize(fig, samples, energy)
-        if show:
-            plt.show()
-        if fname:
-            fig.savefig(os.path.join(RUN_DIR, f"samples.png"))
-        plt.close(fig)
-
-    def save_model(net):
-        ckpt_fn = os.path.join(RUN_DIR, f"model.pkl")
-        torch.save({
-                "epoch": num_epochs,
-                "model": net.state_dict(),
-                "optimizer": optimizer.state_dict()
-            }, ckpt_fn
-        )
-
-    ais_loss = AISLoss(logger=tune.track.log, log_z_update_interval=9)
-
-    net, optimizer = train(net, dataset, num_epochs=num_epochs,
-                           optimizer=optimizer,
-                           num_mc_steps=num_mc_steps,
-                           step_callbacks=[ais_loss])
-    save_model(net=net)
-    save_images(net=net)
 
 
 if __name__ == '__main__':
     analysis = tune.run(
-        training_entrypoint,
+        get_energy_trainer(setup_sm_digits),
         config={
             "lr": tune.loguniform(1e-4, 1e-1),
-            "num_epochs": tune.randint(10, 500),
+            "num_epochs": tune.randint(10, 20),
             "num_mc_steps": tune.randint(1, 20),
-            "n_hidden": tune.randint(4, 128),
+            "n_hidden": tune.randint(4, 48),
             "model": tune.choice(["conv", "resnet"])
         },
         scheduler=ASHAScheduler(metric="loss_ais", mode="min"),
-        num_samples=20
+        num_samples=2,
+        checkpoint_freq=1,
+        checkpoint_at_end=True,
     )
     print(analysis.get_best_config(metric="loss_ais", mode="min"))
